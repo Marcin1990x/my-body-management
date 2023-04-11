eca1ec6fb47df2d52bd3098606d42ad4668de902
Marcin1990x <marcingothico14>
2023-04-11 17:09:43 +0200

    First commit

---
+++ src/main/java/pl/koneckimarcin/mybodymanagement/MyBodyManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.mybodymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBodyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBodyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/koneckimarcin/mybodymanagement/MyBodyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.mybodymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBodyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
