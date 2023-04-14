f5f1fa7f8319475d71a50e35cd9aa70953b2c997
Marcin1990x <marcingothico14>
2023-04-14 18:24:13 +0200

    Added /h2-console access using HttpSecurity

--- src/main/java/pl/koneckimarcin/mybodymanagement/security/SpringSecurityConfiguration.java
+++ src/main/java/pl/koneckimarcin/mybodymanagement/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package pl.koneckimarcin.mybodymanagement.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -38,6 +41,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    // give access to /h2-console
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
     }
 
 }
