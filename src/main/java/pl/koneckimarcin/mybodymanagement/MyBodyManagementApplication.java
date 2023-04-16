c22587c441ddc43e03c1abbad52088e96b84b062
Marcin1990x <marcin.konecki21@gmail.com>
2023-04-16 19:51:37 +0200

    Changes for producing WAR

--- src/main/java/pl/koneckimarcin/mybodymanagement/MyBodyManagementApplication.java
+++ src/main/java/pl/koneckimarcin/mybodymanagement/MyBodyManagementApplication.java
@@ -2,9 +2,15 @@ package pl.koneckimarcin.mybodymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyBodyManagementApplication {
+public class MyBodyManagementApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(MyBodyManagementApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBodyManagementApplication.class, args);
